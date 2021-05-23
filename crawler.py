abbca685517bf98b2a9ab2e5294de630ec00ea27
chengmin.z <chengmin.z@outlook.com>
2021-05-24 00:39:21 +0800

    revert to file store

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 import gzip
+from typing import TextIO
 from urllib.request import urlopen
 from urllib.error import URLError
 from urllib.request import Request
@@ -6,69 +7,69 @@ from io import BytesIO
 import re
 import os
 
-headers: dict = {'Accept-Encoding': 'gzip, deflate'}
-site_name: str = 'http://www.zhongyoo.com'
-index_file = open(os.getcwd() + '/results/' + 'index' + '.txt', 'w')
 
+class Crawler:
 
-def get_website(url: str, header: dict):
-    try:
-        response: Request = Request(url, headers=header)
-        html_bytes: bytes = urlopen(response).read()
-        buff: BytesIO = BytesIO(html_bytes)
-        f = gzip.GzipFile(fileobj=buff)
-        result: str = f.read().decode('gbk', errors='replace')
-    except URLError as error:
-        print('Downloading error: ' + error.reason)
-        result = ''
-    return result
+    def __init__(self):
+        self.__headers: dict = {'Accept-Encoding': 'gzip, deflate'}
+        self.__site_name: str = 'http://www.zhongyoo.com'
+        self.__index_file: TextIO = open(os.getcwd() + '/results/' + 'index.txt', 'w')
 
+    def __get_website(self, url: str):
+        try:
+            response: Request = Request(url, headers=self.__headers)
+            html_bytes: bytes = urlopen(response).read()
+            buff: BytesIO = BytesIO(html_bytes)
+            f = gzip.GzipFile(fileobj=buff)
+            result: str = f.read().decode('gbk', errors='replace')
+        except URLError as error:
+            print('Downloading error: ' + error.reason)
+            result = ''
+        return result
 
-def process_pages_nav(pages: list):
-    name_pattern: str = 'alt=\'(.*)\''
-    detail_page_pattern: str = '<div class="sp"><span class="pics pics2"> <a href=\'(.*)\' class=\'title\'>'
-    total_page = len(pages)
-    for index, page in enumerate(pages):
-        progress: str = format(float(index)/float(total_page), '.2f')
-        print('progress: ' + progress + ' ' + str(index) + ' of ' + str(total_page))
-        current_link: str = site_name + '/name/' + page
-        current_html: str = get_website(current_link, headers)
-        name_results: list = re.findall(name_pattern, current_html)
-        detail_links: list = re.findall(detail_page_pattern, current_html)
-        if len(name_results) == len(detail_links):
-            for index_y, detail_link in enumerate(detail_links):
-                process_detail_page(detail_link, name_results[index_y])
-    print('get files complete')
+    def __process_pages_nav(self, pages: list):
+        name_pattern: str = 'alt=\'(.*)\''
+        detail_page_pattern: str = '<div class="sp"><span class="pics pics2"> <a href=\'(.*)\' class=\'title\'>'
+        total_page = len(pages)
+        for index, page in enumerate(pages):
+            progress: str = format(float(index)/float(total_page), '.2f')
+            print('progress: ' + progress + ' ' + str(index) + ' of ' + str(total_page))
+            current_link: str = self.__site_name + '/name/' + page
+            current_html: str = self.__get_website(current_link)
+            name_results: list = re.findall(name_pattern, current_html)
+            detail_links: list = re.findall(detail_page_pattern, current_html)
+            if len(name_results) == len(detail_links):
+                for index_y, detail_link in enumerate(detail_links):
+                    self.__process_detail_page(detail_link, name_results[index_y])
+        print('get files complete')
 
+    def __process_detail_page(self, link: str, name: str):
+        print('process: ' + link)
+        detail_html: str = self.__get_website(link)
+        image_pattern: str = 'src="(.*).jpg"'
+        image_results: list = re.findall(image_pattern, detail_html)
+        dr = re.compile(r'<[^>]+>', re.S)
+        html_text = dr.sub('', detail_html)
+        start_index: int = html_text.find('show_view_05();')
+        end_index: int = html_text.find('show_view_04();')
+        text_result: str = html_text[start_index+15:end_index]
+        clear_text = text_result.strip().replace('\r', '').replace('\u3000', '').replace('\t', '').split('\n')
+        final_text = ''
+        for line_text in clear_text:
+            if line_text != '':
+                final_text += line_text + '\n'
+        final_text = final_text[0:len(final_text)-1]
+        print('generate: ' + os.getcwd() + '/results/' + name + '.txt')
+        f = open(os.getcwd() + '/results/' + name + '.txt', 'w')
+        f.write(final_text)
+        self.__index_file.write(name + ' ' + link + ' ')
+        if len(image_results) != 0:
+            self.__index_file.write(self.__site_name + image_results[0] + '.jpg' + '\n')
+        else:
+            self.__index_file.write('\n')
 
-def process_detail_page(link: str, name: str):
-    detail_html: str = get_website(link, headers)
-    # print(detail_html)
-    image_pattern: str = 'src="(.*).jpg"'
-    image_results: list = re.findall(image_pattern, detail_html)
-    dr = re.compile(r'<[^>]+>', re.S)
-    html_text = dr.sub('', detail_html)
-    start_index: int = html_text.find('show_view_05();')
-    end_index: int = html_text.find('show_view_04();')
-    text_result: str = html_text[start_index+15:end_index]
-    clear_text = text_result.strip().replace('\r', '').replace('\u3000', '').replace('\t', '').split('\n')
-    final_text = ''
-    for line_text in clear_text:
-        if line_text != '':
-            final_text += line_text + '\n'
-    final_text = final_text[0:len(final_text)-1]
-    print('generate: ' + os.getcwd() + '/results/' + name + '.txt')
-    f = open(os.getcwd() + '/results/' + name + '.txt', 'w')
-    f.write(final_text)
-    index_file.write(name + ' ' + link + ' ')
-    if len(image_results) != 0:
-        index_file.write(site_name + image_results[0] + '.jpg' + '\n')
-    else:
-        index_file.write('\n')
-
-
-def process():
-    html: str = get_website(site_name + '/name', headers)
-    pages_pattern: str = '<option value=\'(.*)\''
-    pages_link: list = re.findall(pages_pattern, html)
-    process_pages_nav(pages_link)
+    def process(self):
+        html: str = self.__get_website(self.__site_name + '/name')
+        pages_pattern: str = '<option value=\'(.*)\''
+        pages_link: list = re.findall(pages_pattern, html)
+        self.__process_pages_nav(pages_link)
--- main.py
+++ main.py
@@ -2,30 +2,38 @@ import logging
 import signal
 
 import retrieval
-import crawler
+from crawler import Crawler
+import dao
 
 from sanic import Sanic
 from sanic import response
+from sanic import exceptions
 
 
 app = Sanic('Remote')
-app.config.DB_NAME = 'user.db'
+
+
+@app.exception(exceptions.NotFound)
+async def ignore_404(request, exception):
+    return response.text('errorUrl', status=404)
 
 
 @app.post('/query')
-async def add_user(request):
+async def query(request):
     query_str: str = request.json.get('input')
     result = retrieval.retrieval_query(query_str)
     return response.json({'code': 200, 'msg': 'success', 'data': result}, status=200)
 
 
-if __name__ == '__main__':
-    need_get_file: bool = False
+def main():
+    need_get_file: bool = True
     if need_get_file:
-        crawler.process()
+        Crawler().process()
+
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-    _logFmt = logging.Formatter('%(ascTime)s %(levelName).1s %(lineno)-3d %(funcName)-20s %(message)s', datefmt='%H:%M:%S')
+    _logFmt = logging.Formatter('%(ascTime)s %(levelName).1s %(lineno)-3d %(funcName)-20s %(message)s',
+                                datefmt='%H:%M:%S')
     _consoleHandler = logging.StreamHandler()
     _consoleHandler.setLevel(logging.DEBUG)
     _consoleHandler.setFormatter(_logFmt)
@@ -35,3 +43,7 @@ if __name__ == '__main__':
     log.setLevel(logging.DEBUG)
 
     app.run(host='127.0.0.1', port=8080)
+
+
+if __name__ == '__main__':
+    main()
